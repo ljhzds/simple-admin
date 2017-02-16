5d35a73dedc6949c5bbe5488812bcade3f6702de
zhangdesheng <jzhangdesheng@gmail.com>
2017-02-16 16:41:07 +0800

    change data

--- admin-rest/ext.py
+++ admin-rest/ext.py
@@ -16,7 +16,7 @@ def make_users():
         user['id'] = uuid.uuid4().__str__().replace('-', '')[:10]
         user['name'] = ''.join([random.choice(xing), random.choice(ming)])
         user['sex'] = random.choice([0, 1])
-        user['branch'] = random.choice(['广发', '国寿'])
+        user['branch'] = random.choice(['机构1', '机构2'])
         user['role'] = random.choice(['管理员', '操作员'])
         users.append(user)
     # data = {'users': users}
@@ -25,13 +25,13 @@ def make_users():
 
 def make_branchs():
     branchs = list()
-    branchs_no = [('100000', '广发'), ('200000', '国寿')]
+    branchs_no = [('100000', '机构1'), ('200000', '机构2')]
     for i in range(2):
         branch = dict()
         branch['branchNo'] = branchs_no[i][0]
         branch['branchName'] = branchs_no[i][1]
         branch['preBranchNo'] = branchs_no[i][0]
-        branch['branchType'] = '总行'
+        branch['branchType'] = '管理机构'
         branchs.append(branch)
     return branchs
 
@@ -42,9 +42,9 @@ def make_nodes():
         node = dict()
         node['nodeId'] = i+1
         node['nodeName'] = '节点'+str(i+1)
-        node['nodeIP'] = '21.96.51.{}'.format(127+i)
-        node['nodePORT'] = '8089'
-        node['nodeType'] = '记账节点'
+        node['nodeIP'] = 'xx.xx.xx.{}'.format(127+i)
+        node['nodePORT'] = '8080'
+        node['nodeType'] = '普通节点'
         node['nodeStat'] = '正常'
         nodes.append(node)
     return nodes
